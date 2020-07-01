45a1ecb14de467d55537cce0e86deb02104233a8
farnabaz-t <farnabaz@gmail.com>
2020-07-01 15:59:07 +0430

    Inject module in binary file

--- bin/nuxt-storybook.js
+++ bin/nuxt-storybook.js
@@ -8,25 +8,21 @@ const { loadNuxt, getBuilder } = require('nuxt-edge')
 const consola = require('consola')
 const packageJson = require('../package.json')
 
-// Resolve rootDir
-let rootDir = '.'
-if (fs.existsSync(process.argv[2])) {
-  rootDir = process.argv.splice(2, 1)[0]
-}
-rootDir = path.resolve(process.cwd(), rootDir)
-
-// Try to guess configDir
-let configDir = path.resolve(rootDir, '.nuxt-storybook', 'storybook')
-if (!fs.existsSync(configDir)) { configDir = undefined }
-
 async function main () {
+  // Resolve rootDir
+  let rootDir = '.'
+  if (fs.existsSync(process.argv[2])) {
+    rootDir = process.argv.splice(2, 1)[0]
+  }
+  rootDir = path.resolve(process.cwd(), rootDir)
   const nuxt = await loadNuxt({
     rootDir,
     for: 'build',
 
     // TODO: find better solution to support all versions of Nuxt
     configOverrides: {
-      buildDir: path.resolve(rootDir, '.nuxt-storybook')
+      buildDir: path.resolve(rootDir, '.nuxt-storybook'),
+      modules: [{ handler: require('../') }]
     }
   })
 
@@ -41,7 +37,7 @@ async function main () {
     ...vueOptions,
     packageJson,
     rootDir,
-    configDir,
+    configDir: path.resolve(rootDir, '.nuxt-storybook', 'storybook'),
     nuxtWebpackConfig,
     frameworkPresets: [
       ...vueOptions.frameworkPresets
--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
   components: true,
-  modules: [
-    { handler: require('../') }
-  ],
   storybook: {
   }
 }
