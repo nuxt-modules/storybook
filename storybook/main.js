beb39a5638a746d61c505b6ee3b0365702803414
farnabaz-t <farnabaz@gmail.com>
2020-07-06 17:57:50 +0430

    support user-defined config

--- storybook/main.js
+++ storybook/main.js
@@ -1,1 +1,8 @@
-module.exports = {}
+const config = require('../../nuxt.config').default
+const storybook = config.storybook || {}
+
+module.exports = {
+  webpackFinal: storybook.webpackFinal,
+  stories: storybook.stories,
+  addons: storybook.addons
+}
