8acee646b80596bef99bf86f21106b7f99d0cd37
farnabaz-t <farnabaz@gmail.com>
2020-07-07 13:29:37 +0430

    Add Support for custome storybook config

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import path from 'path'
+import fs from 'fs'
 import vueOptions from '@storybook/vue/dist/server/options'
 import { buildDev } from '@storybook/core/server'
 import { requireMaybeEdge } from './utils'
 import { StorybookOptions } from './types'
 
 export async function start (options: StorybookOptions) {
-  const configDir = path.resolve(options.rootDir, '.nuxt-storybook', 'storybook')
-
   const {
-    nuxt,
+    // nuxt,
     nuxtBuilder,
-    nuxtWebpackConfig
+    nuxtWebpackConfig,
+    nuxtStorybookConfig
   } = await buildNuxt(options)
+
+  nuxtStorybookConfig.configDir = path.resolve(options.rootDir, '.nuxt-storybook', 'storybook')
 
   const buildOptions = {
     ...vueOptions,
     packageJson: require('../package.json'),
     rootDir: options.rootDir,
-    configDir,
+    configDir: nuxtStorybookConfig.configDir,
     nuxtBuilder,
     nuxtWebpackConfig,
-    nuxtStorybookConfig: nuxt.options.storybook || {},
+    nuxtStorybookConfig,
     frameworkPresets: [
       ...vueOptions.frameworkPresets,
       require.resolve('./preset')
@@ -43,19 +45,11 @@ export async function buildNuxt (options: StorybookOptions) {
       buildDir
     }
   })
-  const templatesRoot = path.resolve(__dirname, '../storybook')
 
-  // Generate storybook files
-  nuxt.moduleContainer.addTemplate({
-    src: path.resolve(templatesRoot, 'main.js'),
-    fileName: path.join('storybook', 'main.js'),
-    options: nuxt.options.storybook || {}
-  })
-  nuxt.moduleContainer.addTemplate({
-    src: path.resolve(templatesRoot, 'preview.js'),
-    fileName: path.join('storybook', 'preview.js'),
-    options: nuxt.options.storybook || {}
-  })
+  const nuxtStorybookConfig = nuxt.options.storybook || {}
+  if (!fs.existsSync(path.resolve(options.rootDir, 'storybook'))) {
+    generateStorybookFiles.call(nuxt.moduleContainer, nuxtStorybookConfig)
+  }
 
   // Create new builder
   const nuxtBuilder = await getBuilder(nuxt)
@@ -73,6 +67,21 @@ export async function buildNuxt (options: StorybookOptions) {
   return {
     nuxt,
     nuxtBuilder,
-    nuxtWebpackConfig
+    nuxtWebpackConfig,
+    nuxtStorybookConfig
   }
+}
+
+function generateStorybookFiles (options) {
+  const templatesRoot = path.resolve(__dirname, '../storybook')
+  this.addTemplate({
+    src: path.resolve(templatesRoot, 'main.js'),
+    fileName: path.join('storybook', 'main.js'),
+    options
+  })
+  this.addTemplate({
+    src: path.resolve(templatesRoot, 'preview.js'),
+    fileName: path.join('storybook', 'preview.js'),
+    options
+  })
 }
