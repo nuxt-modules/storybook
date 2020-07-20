d64dd26e99d02d8f51471abad5fee4b9dfe4f3fb
Farnabaz <farnabaz@gmail.com>
2020-07-20 15:22:26 +0430

    chore: improve tailwind example

--- examples/tailwind/components/MyButton.stories.js
+++ examples/tailwind/components/MyButton.stories.js
@@ -1,3 +1,5 @@
 export default { title: 'Button' }
 
-export const RoundBlue = () => '<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Button</button>'
+export const primaryButton = () => '<MyButton>Primary Button</MyButton>'
+export const secondaryButton = () => '<MyButton type="secondary">Secondary Button</MyButton>'
+export const roundButton = () => '<MyButton round>Round Button</MyButton>'
---
+++ examples/tailwind/components/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="font-bold py-2 px-4 text-white" :class="btnClass">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    round: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  computed: {
+    btnClass () {
+      const classes = []
+      if (this.round) {
+        classes.push('rounded-full')
+      }
+      if (this.type === 'primary') {
+        classes.push('bg-blue-500 hover:bg-blue-700')
+      } else if (this.type === 'secondary') {
+        classes.push('bg-gray-500 hover:bg-gray-700')
+      }
+      return classes.join(' ')
+    }
+  }
+}
+</script>
