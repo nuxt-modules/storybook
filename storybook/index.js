b93669be4eb5b9c33508dcedf3773c4cc4adcefa
Farnabaz <farnabaz@gmail.com>
2020-08-11 13:52:28 +0430

    fix(storybook): windows path resolve

--- storybook/index.js
+++ storybook/index.js
@@ -1,11 +1,6 @@
 import './mock'
 // helpers to fix windows path
 // https://github.com/nuxt/nuxt.js/blob/1edac29eba1a621339105f5adef73d4c30388fee/packages/utils/src/resolve.js#L13
-export const isWindows = process.platform.startsWith('win')
-
 export const wp = function wp (p = '') {
-  if (isWindows) {
-    return p.replace(/\\/g, '\\\\')
-  }
-  return p
+  return p.replace(/\\/g, '\\\\')
 }
