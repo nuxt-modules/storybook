d2a0121d93da165a6774ffa2373cfc17d5814307
Farnabaz <farnabaz@gmail.com>
2020-09-07 16:41:40 +0430

    fix(addon-docs): support nuxt context in docs canvas

--- storybook/preview.js
+++ storybook/preview.js
@@ -1,7 +1,46 @@
+import React from 'react'
 import Vue from 'vue'
 import '~storybook';
 
 <% if (options.nuxtOptions.components) { %>import * as components from '../components';
-Object.keys(components).forEach(name => Vue.component(name, components[name]))<% } %>
+Object.keys(components).forEach(name => Vue.component(name, components[name]))<% } %>;
 
-export const parameters = <%= devalue(options.parameters) %>
+
+// based on: https://github.com/storybookjs/storybook/blob/master/addons/docs/src/frameworks/vue/prepareForInline.ts
+const COMPONENT = 'STORYBOOK_COMPONENT';
+const VALUES = 'STORYBOOK_VALUES';
+const prepareForInline = (storyFn, { args }) => {
+  const component = storyFn()
+  const el = React.useRef(null)
+  // FIXME: This recreates the Vue instance every time, which should be optimized
+  React.useEffect(() => {
+    let root
+    const __NUXT_APP = Promise.resolve(window.__NUXT_APP || require('../').createApp(null, window.__NUXT__.config))
+    __NUXT_APP.then(({ app }) => {
+      root = new Vue({
+        ...app,
+        el: el.current,
+        data () {
+          return {
+            [COMPONENT]: component,
+            [VALUES]: args
+          }
+        },
+        render (h) {
+          const children = this[COMPONENT] ? [h(this[COMPONENT])] : undefined
+          return h('div', { attrs: { id: 'root' } }, children)
+        }
+      })
+    })
+    return () => root && root.$destroy()
+  })
+
+  return React.createElement('div', null, React.createElement('div', { ref: el }))
+}
+
+const globalParameters = <%= devalue(options.parameters) %>;
+globalParameters.docs = {
+    ...globalParameters.docs,
+    prepareForInline
+}
+export const parameters = globalParameters
