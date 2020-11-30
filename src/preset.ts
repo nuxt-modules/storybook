33beee1105cd1173fc8ffab13aabf2eea57e8775
Ahad Birang <farnabaz@gmail.com>
2020-11-30 20:25:43 +0330

    fix(module): move aliases to preset (#184)

--- src/preset.ts
+++ src/preset.ts
@@ -1,3 +1,18 @@
+import path from 'path'
+import * as webpack from 'webpack'
+import { WebpackExtras } from './types'
+
 export default {
-  addons: []
+  webpackFinal (config: webpack.Configuration, extras: WebpackExtras): webpack.Configuration {
+    // Aliases
+    const buildDir = extras.nuxt.options.buildDir
+    config.resolve.alias = {
+      ...extras.nuxtWebpackConfig.resolve.alias,
+      ...config.resolve.alias,
+      '~storybook': path.resolve(__dirname, '../storybook'),
+      // Nuxt plugins alias
+      ...extras.nuxtBuilder.plugins.reduce((map, plugin) => ({ ...map, [plugin.name]: path.resolve(buildDir, plugin.src) }), {})
+    }
+    return config
+  }
 }
