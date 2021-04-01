0daffdbc4d759ef8432cbb67ff0693b3ef45fe02
Farnabaz <farnabaz@gmail.com>
2021-04-01 20:54:59 +0430

    chore: upgrade to postcss 8

--- storybook/main.js
+++ storybook/main.js
@@ -1,5 +1,13 @@
 const stories = [<%= options.stories.map(s => `'${s}'`).join(",") %>]
-const addons  = [<%= options.addons.map(s => devalue(s)).join(",") %>]
+const addons  = [<%= options.addons.map(s => devalue(s)).join(",") %>,{
+  name: '@storybook/addon-postcss',
+  options: {
+    postcssLoaderOptions: {
+      implementation: require('postcss')
+    }
+  }
+}
+]
 
 function nuxifyStorybook(storybookConfig) {
   return {
@@ -11,6 +19,29 @@ function nuxifyStorybook(storybookConfig) {
       if (typeof storybookConfig.webpackFinal === 'function') {
         config = storybookConfig.webpackFinal(config, options)
       }
+      config.module.rules.push({
+        test: /\.css$/,
+        loaders: [
+          // Loader for webpack to process CSS with PostCSS
+          {
+            loader: 'postcss-loader',
+            options: {
+              /* 
+                Enable Source Maps
+               */
+              sourceMap: true,
+              /*
+                Set postcss.config.js config path && ctx 
+               */
+              config: {
+                path: './.storybook/',
+              },
+            },
+          },
+        ],
+    
+        include: path.resolve(__dirname, '../'),
+      });
       return config
     },
     stories: [
