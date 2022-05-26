d64b500ea191a3640831e44741604efb36c4be36
Farnabaz <farnabaz@gmail.com>
2022-05-27 00:43:30 +0200

    chore: upgrade storybook to v6.5.5

--- build.config.ts
+++ build.config.ts
@@ -3,8 +3,5 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     { input: 'src/', outDir: 'dist', format: 'cjs', declaration: false }
-  ],
-  externals: [
-    '@storybook/core'
   ]
 })
