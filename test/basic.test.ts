14b58b34e7d7c339204647022e917f817880c87c
Chakir QATAB <javachakir@gmail.com>
2023-09-30 00:37:22 +0400

    chore(test):added simple test (#433)

---
+++ test/basic.test.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'node:url'
+import { describe, expect, it } from 'vitest'
+import { $fetch, setup } from '@nuxt/test-utils'
+
+describe('ssr', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+  })
+
+  it('renders the index page', async () => {
+    // Get response to a server-rendered page with `$fetch`.
+    const html = await $fetch('/')
+    console.log(html)
+    expect(html).toContain('Welcome to Nuxt!')
+  })
+})
