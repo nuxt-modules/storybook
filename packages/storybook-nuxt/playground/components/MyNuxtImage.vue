bd35181b469d5f5cf7290d16636a9fcdc9f6b8d4
chakAs3 <javachakir@gmail.com>
2024-04-19 10:42:07 +0400

    update playground

--- packages/storybook-nuxt/playground/components/MyNuxtImage.vue
+++ packages/storybook-nuxt/playground/components/MyNuxtImage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NuxtImg
-      src="/images/sb-nuxt-logo.png"
+      src="/images/sb-nuxt-logo.jpg"
       :placeholder="[30, 20]"
       width="300"
       height="200"
@@ -12,7 +12,7 @@
       responsive image with nuxt-image component:
     </h4>
     <NuxtPicture
-      src="/images/sb-nuxt-logo.png"
+      src="/images/sb-nuxt-logo.jpg"
       width="1204"
       height="632"
       sizes="xs:200px md:500px lg:1024"
