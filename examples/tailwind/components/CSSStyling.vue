5011f93a62be120398a1feb0d23c3d3921ee3486
Tobias Diez <code@tobiasdiez.de>
2024-04-24 17:10:34 +0800

    chore: add example for usage with tailwind

---
+++ examples/tailwind/components/CSSStyling.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4">
+    <div class="px-6 py-4">
+      <span class="badge mr-2">#photography</span>
+      <span class="badge mr-2">#travel</span>
+      <span class="badge">#winter</span>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
---
+++ examples/tailwind/components/SimpleDiv.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="px-6 py-4 pb-2">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+      quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+      nihil.
+    </p>
+  </div>
+</template>
