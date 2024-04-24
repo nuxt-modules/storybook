bc74285a772e5fc8a2e2863691dae6d3846531e9
Tobias Diez <code@tobiasdiez.de>
2024-04-24 18:10:56 +0800

    chore: add example for usage with tailwind (#565)

---
+++ examples/tailwind/components/CSSStyling.stories.ts
@@ -0,0 +1,13 @@
+import CSSStyling from './CSSStyling.vue'
+
+const meta = {
+  title: 'Example/CSS Styling',
+  component: CSSStyling,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+export const CSSStylingStory = {
+  args: {},
+}
---
+++ examples/tailwind/components/SimpleDiv.stories.ts
@@ -0,0 +1,13 @@
+import SimpleDiv from './SimpleDiv.vue'
+
+const meta = {
+  title: 'Example/Simple Div',
+  component: SimpleDiv,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+export const SimpleDivStory = {
+  args: {},
+}
