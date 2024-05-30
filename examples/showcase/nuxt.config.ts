a6cf2969e1430ab669c0560147af5742fe812e66
Tobias Diez <code@tobiasdiez.de>
2024-05-30 19:02:19 +0800

    add runtime config

--- examples/showcase/nuxt.config.ts
+++ examples/showcase/nuxt.config.ts
@@ -2,4 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/storybook'],
+  runtimeConfig: {
+    // For testing runtimeConfig in useMyComposable
+    app: {
+      name: 'Nuxt',
+      version: '1.0.0',
+      baseURL: '/',
+      host: 'localhost',
+      port: 3000,
+    },
+  },
 })
