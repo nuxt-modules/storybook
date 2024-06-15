06d559773d46c3d5fcaa1ed0bc848d93b1292d59
Tobias Diez <code@tobiasdiez.de>
2024-06-15 18:53:37 +0800

    add temporary workaround for https://github.com/nuxt/nuxt/issues/27490

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   modules: ['@nuxtjs/plausible'],
+  // Workaround for https://github.com/nuxt/nuxt/issues/27490
+  nitro: {
+    prerender: {
+      routes: ['/'],
+    },
+  },
 })
