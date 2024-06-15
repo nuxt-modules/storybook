70db8af8d7d72f7b149fa48069f759e732198fd7
Ryan Leckey <ryan@calaxy.com>
2024-06-15 10:57:45 -0700

    fix: extend storybook config from vue3-vite framework
    Signed-off-by: Ryan Leckey <ryan@calaxy.com>

--- packages/storybook-nuxt/src/types.d.ts
+++ packages/storybook-nuxt/src/types.d.ts
@@ -1,7 +1,4 @@
-import type {
-  BuilderOptions,
-  StorybookConfig as StorybookConfigBase,
-} from '@storybook/types'
+import type { StorybookConfig as StorybookConfigBase } from '@storybook/types'
 import type {
   Preview,
   StoryFn,
@@ -10,6 +7,7 @@ import type {
   Meta,
   DecoratorFunction,
 } from '@storybook/vue3'
+import type { FrameworkOptions as FrameworkOptionsVue } from '@storybook/vue3-vite'
 
 declare let STORYBOOK_VUE_GLOBAL_PLUGINS: string[]
 declare let STORYBOOK_VUE_GLOBAL_MIXINS: string[]
@@ -17,9 +15,7 @@ declare let STORYBOOK_VUE_GLOBAL_MIXINS: string[]
 type FrameworkName = '@storybook-vue/nuxt'
 type BuilderName = '@storybook/builder-vite'
 
-export type FrameworkOptions = NuxtOptions & {
-  builder?: BuilderOptions
-}
+export type FrameworkOptions = NuxtOptions & FrameworkOptionsVue
 
 type StorybookConfigFramework = {
   framework: FrameworkName | { name: FrameworkName; options: FrameworkOptions }
@@ -30,11 +26,17 @@ type StorybookConfigFramework = {
   addons?: StorybookConfigBase['addons']
   docs?: StorybookConfigBase['docs']
 }
+
 /**
  * The interface for Storybook configuration in `main.ts` files.
  */
-export type StorybookConfig = {
-  viteFinal?: Record<string, unknown>
-} & StorybookConfigFramework
+export type StorybookConfig = Omit<
+  StorybookConfigBase,
+  keyof StorybookConfigVite | keyof StorybookConfigFramework
+> &
+  StorybookConfigVite &
+  StorybookConfigFramework
+
 export interface NuxtOptions {}
+
 export { Meta, StoryFn, StoryObj, Preview, VueRenderer, DecoratorFunction }
