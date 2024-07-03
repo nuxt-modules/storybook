59086374afb64658a83ce9c98554331682d47b7d
Tobias Diez <code@tobiasdiez.de>
2024-07-03 13:44:37 +0200

    fix prettier

--- docs/app/pages/[...slug].vue
+++ docs/app/pages/[...slug].vue
@@ -67,7 +67,7 @@ const links = computed(() =>
     <UPageBody prose>
       <ContentRenderer v-if="page.body" :value="page" />
 
-      <hr v-if="surround?.length" >
+      <hr v-if="surround?.length" />
 
       <UContentSurround :surround="surround" />
     </UPageBody>
