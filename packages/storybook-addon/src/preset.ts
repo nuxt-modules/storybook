cb49e70b74a2a345b366341d9420e0fcfdd359d6
Tobias Diez <code@tobiasdiez.de>
2024-12-09 10:22:04 +0800

    Remove storybook:vue-template-compilation

--- packages/storybook-addon/src/preset.ts
+++ packages/storybook-addon/src/preset.ts
@@ -159,6 +159,16 @@ function mergeViteConfig(
     plugins.unshift(vuePlugin())
   }
 
+  // Remove the 'storybook:vue-template-compilation' plugin because it yields out-of-memory issues
+  const templatePluginIndex = plugins?.findIndex(
+    (plugin) =>
+      plugin &&
+      'name' in plugin &&
+      plugin.name === 'storybook:vue-template-compilation',
+  )
+  if (templatePluginIndex !== -1) {
+    plugins.splice(templatePluginIndex, 1)
+  }
   extendedConfig.plugins = plugins
 
   // Storybook adds 'vue' as dependency that should be optimized, but nuxt explicitly excludes it from pre-bundling
