660d5243fe9937238b597777da6870605066fdee
chakAs3 <javachakir@gmail.com>
2024-12-22 18:10:51 +0400

    feat: add i18n support with multiple languages and example stories to playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['../packages/nuxt-module/src/module', '@nuxt/test-utils/module'],
+  modules: ['../packages/nuxt-module/src/module', '@nuxt/test-utils/module', '@nuxtjs/i18n'],
 
   storybook: {
     // Very verbose logs for debugging
     logLevel: Number.POSITIVE_INFINITY,
+  },
+  
+  i18n: {
+    locales: ['en', 'fr', 'ar'],
+    defaultLocale: 'en',
   },
 
   compatibilityDate: '2024-08-03',
