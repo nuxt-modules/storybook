8bc676a7ff33009a7d0034f43a7361facc8bbbfc
Nestor Vera <nestorvera@me.com>
2025-07-14 12:16:14 +0200

    fix: ci not passing due to formatting issues

--- examples/showcase/components/NuxtFonts.vue
+++ examples/showcase/components/NuxtFonts.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="font-size: 4rem; line-height: 1;">
+  <div style="font-size: 4rem; line-height: 1">
     <p class="font-roboto">Hamburgefonstiv</p>
     <p class="font-manufacturing-consent">Hamburgefonstiv</p>
     <p class="font-inter">Hamburgefonstiv</p>
@@ -7,7 +7,13 @@
 </template>
 
 <style scoped>
-.font-roboto { font-family: 'Roboto', sans-serif; }
-.font-manufacturing-consent { font-family: 'Manufacturing Consent', serif; }
-.font-inter { font-family: 'Inter', sans-serif; }
+.font-roboto {
+  font-family: 'Roboto', sans-serif;
+}
+.font-manufacturing-consent {
+  font-family: 'Manufacturing Consent', serif;
+}
+.font-inter {
+  font-family: 'Inter', sans-serif;
+}
 </style>
