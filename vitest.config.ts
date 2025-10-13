18c3b7117eff1e05e4a989084d7ed4e73ab95c94
Francesco Schwarz <francesco.schwarz@posteo.de>
2025-10-13 16:12:07 +0200

    fix: use updated vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,21 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
+import { defineConfig } from 'vitest/config'
 
-export default defineVitestConfig({
+export default defineConfig({
   test: {
-    typecheck: {
-      enabled: true,
-      // Don't fail the test if typecheck of source files fails for the moment
-      ignoreSourceErrors: true,
-    },
-    include: ['test/*.spec.ts'],
+    projects: [
+      await defineVitestProject({
+        test: {
+          typecheck: {
+            enabled: true,
+            // Don't fail the test if typecheck of source files fails for the moment
+            ignoreSourceErrors: true,
+          },
+          name: 'nuxt',
+          include: ['test/*.spec.ts'],
+          environment: 'nuxt',
+        },
+      }),
+    ],
   },
 })
