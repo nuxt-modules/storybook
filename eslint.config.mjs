2d6a6ec95fa3dea4c80406a68200e8794ef8f9a2
Tony Kornmeier <akornmeier@users.noreply.github.com>
2025-11-18 13:21:17 -0500

    chore: prettier check, missed a semi

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
 export default createConfigForNuxt({
   features: {
@@ -11,10 +11,10 @@ export default createConfigForNuxt({
   dirs: {
     src: ['./playground'],
   },
-}).override('nuxt/vue/rules', {
-  rules: {
-    'vue/html-self-closing': 'off',
-  },
-}).append(
-  ...storybook.configs['flat/recommended']
-)
+})
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/html-self-closing': 'off',
+    },
+  })
+  .append(...storybook.configs['flat/recommended'])
