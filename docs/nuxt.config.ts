d355c770bece73cf7bbddb4f284eae1104497c03
Tobias Diez <code@tobiasdiez.de>
2026-03-30 02:20:57 +0200

    chore: remove remaining reference to eslint

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/ui-pro',
     '@nuxt/content',
@@ -47,15 +46,6 @@ export default defineNuxtConfig({
     prerender: {
       routes: ['/'],
       crawlLinks: true,
-    },
-  },
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs',
-      },
     },
   },
 
